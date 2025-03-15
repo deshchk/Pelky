42d577256d82c1815385917d9c7f33ec5dc3876d
Daniel <110098882+deshchk@users.noreply.github.com>
2025-03-15 03:03:21 +0100

    fixed lastAssessed overflow issue

--- src/components/tracker/TrackerList.jsx
+++ src/components/tracker/TrackerList.jsx
@@ -2,6 +2,7 @@ import TrackerItem from "@/components/tracker/TrackerItem"
 import { useEffect, useRef } from "react"
 
 function TrackerList({items, data}) {
+  const listContainer = useRef(null)
   const listEl = useRef(null)
 
   const loadingTimeClasses = items[0] ?
@@ -9,12 +10,15 @@ function TrackerList({items, data}) {
     items[0].priority === 'mid' && '![transition:_grid-template-rows_.2s,_translate_.40s]' : ''
 
   useEffect(() => {
-    listEl.current.classList.remove('hiding-animation')
+    listContainer.current.classList.remove('hiding-animation')
+    setTimeout(() => {
+      listEl.current.classList.remove('hiding')
+    },200)
   }, [])
 
   return (
-    <div className={`hide-able grid-rows-[1fr] empty:!hidden hiding-animation ${loadingTimeClasses}`.trim()} ref={listEl}>
-      {items.length > 0 && <ul className="grid grid-cols-1 gap-4 overflow-hidden">
+    <div className={`hide-able grid-rows-[1fr] empty:!hidden hiding-animation ${loadingTimeClasses}`.trim()} ref={listContainer}>
+      {items.length > 0 && <ul className="grid grid-cols-1 gap-4 [&.hiding]:overflow-hidden hiding" ref={listEl}>
         {items.map(item => (
             <TrackerItem key={item.id} item={item} data={data}>{item.title}</TrackerItem>
         ))}
