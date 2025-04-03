71fd0fac4d3fccdd62f4568dd9ee90ccc9bfe133
Daniel <110098882+deshchk@users.noreply.github.com>
2025-04-03 13:05:54 +0200

    NewItemDialog fixed colors and spacing

--- src/components/dialogs/NewItemDialog.jsx
+++ src/components/dialogs/NewItemDialog.jsx
@@ -60,25 +60,25 @@ function NewItemDialog({handleConfirm, closeDialog}) {
               <Chevron className="group-[:not(.collapsed)]:scale-y-[-1] size-4 stroke-2 pointer-events-none"/>
             </div>
 
-            <div className="hide-able grid-rows-[1fr] peer-[.collapsed]:grid-rows-[0fr] peer-[:not(.collapsed)]:mt-4 peer-[:not(.collapsed)]:mb-5">
+            <div className="hide-able grid-rows-[1fr] peer-[.collapsed]:grid-rows-[0fr] peer-[:not(.collapsed)]:mt-4 peer-[:not(.collapsed)]:mb-1">
               <div className="flex flex-col items-center gap-y-4 font-semibold px-3 touch-manipulation overflow-hidden">
                 <div className="self-stretch grid grid-cols-3 gap-x-3 mt-2">
                   <label htmlFor="scale-both" className="h-6 bg-linear-to-r from-red-500 from-20% to-80% to-green-700 rounded outline-offset-2 opacity-50 has-checked:outline has-checked:outline-late-50 has-checked:opacity-100 transition-opacity">
                     <input id="scale-both" name="scale-type" type="radio" className="sr-only" onChange={() => setScaleType('both')} defaultChecked/>
                   </label>
 
-                  <label htmlFor="scale-positive" className="h-6 bg-linear-to-r from-slate-700 to-80% to-lime-700 rounded outline-offset-2 opacity-50 has-checked:outline has-checked:outline-late-50 has-checked:opacity-100 transition-opacity">
+                  <label htmlFor="scale-positive" className="h-6 bg-linear-to-r from-slate-700 to-80% to-green-700 rounded outline-offset-2 opacity-50 has-checked:outline has-checked:outline-late-50 has-checked:opacity-100 transition-opacity">
                     <input id="scale-positive" name="scale-type" type="radio" className="sr-only" onChange={() => setScaleType('positive')}/>
                   </label>
 
-                  <label htmlFor="scale-negative" className="h-6 bg-linear-to-r from-slate-700 to-80% to-orange-600 rounded outline-offset-2 opacity-50 has-checked:outline has-checked:outline-late-50 has-checked:opacity-100 transition-opacity">
+                  <label htmlFor="scale-negative" className="h-6 bg-linear-to-r from-slate-700 to-80% to-red-500 rounded outline-offset-2 opacity-50 has-checked:outline has-checked:outline-late-50 has-checked:opacity-100 transition-opacity">
                     <input id="scale-negative" name="scale-type" type="radio" className="sr-only" onChange={() => setScaleType('negative')}/>
                   </label>
                 </div>
 
                 <div className={`h-8 flex justify-between items-center px-3 w-10/12 rounded-md transition-colors duration-300 bg-linear-to-r ${
                   scaleType === 'both' ? 'from-red-500 from-20% to-80% to-green-700'
-                    : scaleType === 'positive' ? 'from-slate-700 to-80% to-lime-700' : 'from-slate-700 to-80% to-orange-600'
+                    : scaleType === 'positive' ? 'from-slate-700 to-80% to-green-700' : 'from-slate-700 to-80% to-red-500'
                 }`}>
                   <span>
                     {scaleType === 'both' ? `-${scaleMaxValue}` : 0}
